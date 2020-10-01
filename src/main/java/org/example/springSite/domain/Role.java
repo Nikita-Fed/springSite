a7586ce362342d7507eed9637c0b16ab093f81e2
Nikita Fedotov <nikita-f@ya.ru>
2020-10-01 23:35:52 +0300

    Добавлена система авторизации при помощи Spring Security, а также возможность добавлять пользователей через форму на сайте, а не прописывая их вручную в файле.

---
+++ src/main/java/org/example/springSite/domain/Role.java
@@ -0,0 +1,5 @@
+package org.example.springSite.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/org/example/springSite/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package org.example.springSite.repos;
+
+import org.example.springSite.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
